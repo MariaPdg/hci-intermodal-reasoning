a2cf4dec5f70a85bb1a0b8d43ede8a9a70d81508
mariapdg <mariapdg@minas-tirith.simtech.uni-stuttgart.de>
2019-11-07 15:40:38 +0100

    text network now do inference

--- text_network.py
+++ text_network.py
@@ -1,5 +1,5 @@
 from transformers.modeling_distilbert import DistilBertForSequenceClassification
-from transformers import DistilBertTokenizer
+from transformers import DistilBertTokenizer, DistilBertConfig
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler
 
 import utils
@@ -10,12 +10,16 @@ if __name__ == "__main__":
     train_data = TensorDataset(captions)
     train_sampler = RandomSampler(train_data)
     train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=8, num_workers=2)
+    
+    config = DistilBertConfig.from_pretrained("distilbert-base-uncased")
+    config.output_hidden_states = True
 
     tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
-    model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased")
-
+    model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased", config=config)
+    model.cuda()
+    model.eval()
     for step, batch in enumerate(train_dataloader):
         batch = tuple(t.to("cuda") for t in batch)
-        output = model(batch)
-        print(output)
+        output = model(batch[0])
+        print(len(output))
 
