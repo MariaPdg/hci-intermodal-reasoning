066e7dc48d3db344d63b7ae0840e56c75dbacda9
sontung <tung.nguyenson1404@gmail.com>
2019-11-11 13:26:03 +0100

    mask for captions, cleaner code for network class

--- text_network.py
+++ text_network.py
@@ -2,39 +2,52 @@ from transformers.modeling_distilbert import DistilBertForSequenceClassification
 from transformers import DistilBertTokenizer, DistilBertConfig
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler
 
+import types
 import utils
 import torch
+import torch.nn as nn
+
+
+def forward_layer(self, input_ids,  attention_mask=None, head_mask=None):
+    distilbert_output = self.distilbert(input_ids=input_ids,
+                                        attention_mask=attention_mask,
+                                        head_mask=head_mask)
+    hidden_state = distilbert_output[0]  # (bs, seq_len, dim)
+    pooled_output = hidden_state[:, 0]  # (bs, dim)
+    pooled_output = self.pre_classifier(pooled_output)  # (bs, dim)
+    pooled_output = nn.ReLU()(pooled_output)  # (bs, dim)
+    pooled_output = self.dropout(pooled_output)  # (bs, dim)
+    return pooled_output
 
 
 class TextNet:
-    def __init__(self):
+    def __init__(self, dev="cpu"):
         config = DistilBertConfig.from_pretrained("distilbert-base-uncased")
         config.output_hidden_states = True
 
         self.tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
         self.model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased", config=config)
+        self.model.forward_layer = types.MethodType(forward_layer, self.model)
+        self.model.to(dev)
+
+        self.model.eval()
 
     def forward(self, indices, masks):
         _, hidden_vec = self.model(indices, masks)
+        hidden_vec2 = self.model.forward_layer(indices, masks)
+        print(hidden_vec[-1].size(), hidden_vec2.size())
         
 
 if __name__ == "__main__":
     device = "cpu"
-
+    net = TextNet(device)
     captions = torch.load("cached_data/val_cap")
-    train_data = TensorDataset(captions)
+    masks = torch.load("cached_data/val_mask")
+    train_data = TensorDataset(captions, masks)
     train_sampler = RandomSampler(train_data)
     train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=8, num_workers=2)
-    
-    config = DistilBertConfig.from_pretrained("distilbert-base-uncased")
-    config.output_hidden_states = True
 
-    tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
-    model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased", config=config)
-    model.to(device)
-    model.eval()
     for step, batch in enumerate(train_dataloader):
         batch = tuple(t.to(device) for t in batch)
-        logits, hidden = model(batch[0])
-        print(hidden[0].size())
+        net.forward(batch[0], batch[1])
 
--- vision_network.py
+++ vision_network.py
@@ -2,12 +2,30 @@ import torchvision.models
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torch
+import types
+
+
+def forward_layer(self, y):
+    y = self.conv1(y)
+    y = self.bn1(y)
+    y = self.relu(y)
+    y = self.maxpool(y)
+
+    y = self.layer1(y)
+    y = self.layer2(y)
+    y = self.layer3(y)
+    y = self.layer4(y)
+
+    y = self.avgpool(y)
+    y = torch.flatten(y, 1)
+
+    return y
 
 
 class VisionNet:
     def __init__(self):
         self.model = torchvision.models.resnext101_32x8d(pretrained=True)
-        print(self.model.state_dict)
+        self.model.forward_layer = types.MethodType(forward_layer, self.model)
 
     def forward(self, image):
         return self.model.forward_layer(image)
@@ -31,6 +49,7 @@ if __name__ == "__main__":
         num_workers=2, pin_memory=True)
 
     for step, batch in enumerate(train_loader):
+        print(batch[0].size())
         out = net.forward(batch[0])
         print(out.size())
         break
