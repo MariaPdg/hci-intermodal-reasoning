8b81b4e90fbcdcd95ba313f65bdc86f1c931c120
mariapdg <mariapdg@minas-tirith.simtech.uni-stuttgart.de>
2019-12-02 00:12:41 +0100

    add TextNet with embeddings

--- original/TextNet.py
+++ original/TextNet.py
@@ -13,6 +13,7 @@ import torch.nn.functional as F
 from torchvision import models
 import gensim
 import types
+import numpy as np
 
 from transformers.modeling_distilbert import DistilBertForSequenceClassification
 from transformers import DistilBertTokenizer, DistilBertConfig
@@ -23,39 +24,25 @@ from torch.utils.data import TensorDataset, DataLoader, RandomSampler
 # In[ ]:
 
 
-# class Text_Net(nn.Module):
-
-#     def __init__(self):
-#         super(Text_Net, self).__init__()
-#         self.embedding = nn.Embedding(3, 300)
-#         self.embedding.weight = nn.Parameter(torch.tensor(embedding_matrix, dtype=torch.float32))
-#         self.embedding.weight.requires_grad = False
-#         self.conv1 = nn.Conv1d(in_channels=16, out_channels=300, kernel_size=3)
-#         self.conv2 = nn.Conv1d(in_channels=300, out_channels=300, kernel_size=3)
-#         self.conv3 = nn.Conv1d(in_channels=300, out_channels=300, kernel_size=3)
-#         self.fc1 = nn.Linear(in_features=300*73, out_features=9216)
-
-#     def forward(self, inputs):
-#         out = F.relu(self.conv1(inputs))
-#         out = F.max_pool1d(F.relu(self.conv2(out)), kernel_size=2)
-#         out = F.max_pool1d(F.relu(self.conv3(out)), kernel_size=2)
-#         out = self.fc1(out.view(-1, 300*73))
-#         return out
-
 class Text_Net(nn.Module):
 
-    def __init__(self):
+    def __init__(self, embed_matrix):
+        
         super(Text_Net, self).__init__()
-        self.conv1 = nn.Conv1d(in_channels=52, out_channels=300, kernel_size=3) #how to reshape the input [52,300,3]???
+        self.embedding = nn.Embedding(np.shape(embed_matrix)[0], np.shape(embed_matrix)[1])
+        self.embedding.weight = nn.Parameter(torch.tensor(embed_matrix, dtype=torch.float32))
+        self.embedding.weight.requires_grad = False
+        self.conv1 = nn.Conv1d(in_channels=52, out_channels=300,  kernel_size=3)
         self.conv2 = nn.Conv1d(in_channels=300, out_channels=300, kernel_size=3)
         self.conv3 = nn.Conv1d(in_channels=300, out_channels=300, kernel_size=3)
-        self.fc1 = nn.Linear(in_features=300*73, out_features=9216) #what is 73???
+        self.fc1 = nn.Linear(in_features=300*73, out_features=9216)
 
     def forward(self, inputs):
-        out = F.relu(self.conv1(inputs))
+        out = self.embedding(inputs)  
+#         out = out.unsqueeze(0)
+        out = F.relu(self.conv1(out))
         out = F.max_pool1d(F.relu(self.conv2(out)), kernel_size=2)
         out = F.max_pool1d(F.relu(self.conv3(out)), kernel_size=2)
         out = self.fc1(out.view(-1, 300*73))
         return out
-
 
