45623ba491937f3e66d5a39a5de519c0f4bf3ca1
sontung <tung.nguyenson1404@gmail.com>
2020-01-09 23:16:13 +0100

    plotly code with json input

--- plotting.py
+++ plotting.py
@@ -1,32 +1,43 @@
 import plotly.graph_objects as go
 import numpy as np
 import plotly
-np.random.seed(1)
+import json
+import sys
+import os
+
 
 plotly.io.orca.config.executable = "/home/sontung/Tools/orca"
 
-N = 100
-x = np.random.rand(N)
-y = np.random.rand(N)
-colors = np.random.rand(N)
-sz = np.random.rand(N) * 30
+with open('run-20200107-232324-tag-Accuracy_val.json') as json_file:
+    data = json.load(json_file)
+data_entry1 = [du[2] for du in data]
+
+colors = np.random.rand()
+sz = np.random.rand() * 30
 
 fig = go.Figure()
-fig.add_trace(go.Scatter(
-    x=x,
-    y=y,
-    mode="markers",
-    marker=go.scatter.Marker(
-        size=sz,
-        color=colors,
-        opacity=0.6,
-        colorscale="Viridis"
+fig.add_trace(
+    go.Scatter(
+        x=list(range(len(data_entry1))),
+        y=data_entry1,
+        mode="lines",
+        name="name 1"
     )
-))
+)
+
+fig.update_layout(
+    title="Accuracy on Val set",
+    xaxis_title="epoch",
+    yaxis_title="accuracy",
+    font=dict(
+        family="Courier New, monospace",
+        size=18,
+        color="#7f7f7f"
+    ),
+    showlegend=True
+)
 
 fig.show()
-
-import os
 
 if not os.path.exists("images"):
     os.mkdir("images")
